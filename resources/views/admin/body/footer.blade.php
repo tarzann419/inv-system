cda0e56b093db61d280416f109bf6056d07eedad
Dan Ogbo <108550261+tarzann419@users.noreply.github.com>
2023-02-04 19:52:30 +0100

    Update footer.blade.php

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,11 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                 EasyLearning © Support@easylearningbd.com.
+                                 OgboDan © 
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by EasyLearning
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by OgboDan
                                 </div>
                             </div>
                         </div>
